b315e68fa686684e97647a11a783c8932d7d1f20
black <black@diancun.net>
2020-09-29 23:54:51 +0800

    perf: 两个线程数组打印，查看问题

--- src/bean/master.js
+++ src/bean/master.js
@@ -31,6 +31,15 @@ class Master{
      */
     assignments(data){
         const list = JSON.parse(data);
+        const freeCpu = (this.workCPU.length + this.freeCPU.length);
+        if(freeCpu > this.cpuNum){
+            console.error('线程超标');
+            const workCPUArr = this.workCPU.map(v => v.pid);
+            const freeCPUArr = this.workCPU.map(v => v.pid);
+            console.error('忙碌进程数：',workCPUArr);
+            console.error('空闲进程数：',freeCPUArr);
+            return ;
+        }
         console.log("目前能执行的进程数量:",(this.cpuNum - this.workCPU.length));
         for(let i = 0; i < (this.cpuNum - this.workCPU.length); i++){
             console.log("忙碌机子数量>>>>",this.workCPU.length);
@@ -60,12 +69,12 @@ class Master{
         if(this.freeCPU.length > 0 ){
             work = this.freeCPU[0];
             console.log('使用空闲进程号为',work.pid);
-            this.freeCPU.remove(work);
         }else{
             console.log('创建新的一个子线程,空闲进程数量为',this.freeCPU.length)
             work = childProcess.fork(workPath);
         }
         this.workCPU.push(work);
+        this.freeCPU.remove(work);
         console.log(`>>>>>>新来一个任务，任务类型${info.type} 新建一个线程pid:`, work.pid);
         info.index = index + 1 ;
         work.send(JSON.stringify(info));
